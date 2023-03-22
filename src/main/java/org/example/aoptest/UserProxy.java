a12fb980cbe4b5e357c1ee5193b0b6f44536c031
lixin <lixin@DESKTOP-RVED93C>
2023-03-22 14:59:56 +0800

    AspectJ注解实现AOP操作 相同切入点抽取

--- src/main/java/org/example/aoptest/UserProxy.java
+++ src/main/java/org/example/aoptest/UserProxy.java
@@ -11,33 +11,37 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class UserProxy {
+    //相同切入点抽取 随便定义一个方法
+    @Pointcut(value = "execution(* org.example.aoptest.User.add(..))")
+    private void pointcut(){
+    }
     //前置通知
     //Before注解表示作为前置通知
-    @Before(value = "execution(* org.example.aoptest.User.add(..))")
+    @Before(value = "pointcut()")
     public void before(){
         System.out.println("before...........");
     }
 
     //最终通知 被执行方法执行后 执行
-    @After(value="execution(* org.example.aoptest.User.add(..))")
+    @After(value="pointcut()")
     public void after(){
         System.out.println("after...........");
     }
 
     //后置通知 被执行方法返回结果后才会执行
-    @AfterReturning(value="execution(* org.example.aoptest.User.add(..))")
+    @AfterReturning(value="pointcut()")
     public void afterReturn(){
         System.out.println("afterReturn...........");
     }
 
     //异常通知
-    @AfterThrowing(value="execution(* org.example.aoptest.User.add(..))")
+    @AfterThrowing(value="pointcut()")
     public void afterThrowing(){
         System.out.println("afterThrowing...........");
     }
 
     //环绕通知
-    @Around(value="execution(* org.example.aoptest.User.add(..))")
+    @Around(value="pointcut()")
     public void around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         System.out.println("环绕通知执行前");
         //被执行的方法
